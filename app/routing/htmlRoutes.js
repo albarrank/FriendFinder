47c6e54a68e85e8df368d88967666ff9276b181c
albarrank <albarrankevin97@gmail.com>
2019-04-25 01:05:08 -0700

    cleaned up js code a bit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,14 @@
 const path = require("path");
 
-module.exports = (function() {
-    
-    'use strict';
-    
-    var htmlExternalRouts = require("express").Router();
+var htmlExternalRoutes = require("express").Router();
 
-    htmlExternalRouts.get("/", function(req, res) {
+htmlExternalRoutes.get("/", function(req, res) {
 
-        res.sendFile(path.join(__dirname , "../public/home.html"));
-    });
+    res.sendFile(path.join(__dirname , "../public/home.html"));
+});
 
-    htmlExternalRouts.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
+htmlExternalRoutes.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
 
-    return htmlExternalRouts;
-})();
+module.exports = htmlExternalRoutes
