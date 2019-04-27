2cbcee3887f780d6fd3437a8046bd1d84b10f079
albarrank <albarrankevin97@gmail.com>
2019-04-27 10:02:58 -0700

    finding average of differences of scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,8 +12,83 @@ externalApiRoutes.get("/friends", function(req, res) {
 });
 
 externalApiRoutes.post("/friends", function(req, res) {
-    console.log(req.body);
-    console.log("api post request is working");
+    data.push(req.body);
+
+    function makeArrayOfNewFriendScores() {
+
+        var newFriend = req.body;
+        var numberArray = newFriend.scores;
+
+        getScoreArraysFromData(numberArray);
+    }
+    
+    function getScoreArraysFromData(answerArray) {
+
+        var compareArray = [];
+
+        for (var i = 0; i < data.length - 1; i++) {
+            compareArray.push(data[i].scores);
+        }
+        
+        compareValues(answerArray, compareArray);
+    }
+
+    function compareValues(answerArrayScores, compareArrayScores) {
+        var AvgdifferenceArrays = [];
+
+        for (var i = 0; i < compareArrayScores.length; i++) {
+
+            var selectedArray = compareArrayScores[i];
+            var differenceArrays =[];
+            var transferArray =[];
+            var average = 0;
+            for (var k = 0; k < selectedArray.length; k++) {
+
+                var difference = Math.abs(parseInt(selectedArray[k]) - parseInt(answerArrayScores[k]));
+                differenceArrays.push(difference);
+                average += differenceArrays[k];
+            }
+            transferArray.push(average);
+            AvgdifferenceArrays.push(transferArray);
+        }
+        console.log(AvgdifferenceArrays);
+    }
+
+    makeArrayOfNewFriendScores();
+
+    // function getNewPersonFinalScore () {
+    //     data.push(req.body);
+
+    //     var newPerson = req.body;
+    //     var numbers = newPerson.scores;
+    //     var finalScore = 0;
+
+    //     for (var i = 0; i < numbers.length; i++) {
+    //         finalScore += parseInt(numbers[i]);
+    //     }
+    //     getScoresFromDataObject(finalScore);
+    // }
+
+    // function getScoresFromDataObject(newPersonFinalScore) {
+    //     var resultsArray = [];
+    //     for (var i = 0; i < data.length -1; i++) {
+    //         var scoreArray = data[i].scores;
+    //         var totalResult = 0;
+
+    //         for (var k = 0; k < scoreArray.length; k++) {
+    //             totalResult += parseInt(scoreArray[k]);
+
+    //         }
+    //         resultsArray.push(totalResult);
+    //     }
+    //     compareScores(resultsArray, newPersonFinalScore)
+    // }
+
+    // function compareScores(resultsArray, newPersonFinalScore) {
+
+    // }
+    
+    // getNewPersonFinalScore();
 });
 
 module.exports = externalApiRoutes;
